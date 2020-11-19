12a29a4ca0b0da84bd6e3fb5e75af9fc6d188061
yiiforces <30460340+yiiforces@users.noreply.github.com>
2020-11-19 07:24:25 -0400

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,11 +1,10 @@
 <?php
-define('DS', DIRECTORY_SEPARATOR);
 
 // tmp dev autoload without composer
 spl_autoload_register(function($className) {
 
-	$file = sprintf('%s/%s.php', __DIR__ . DS, $className);
-	$file = str_replace('\\', DS, $file);
+	$file = sprintf('%s/%s.php', __DIR__ . DIRECTORY_SEPARATOR, $className);
+	$file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
 	$file = str_replace('//', '/', $file);
 
 	if(file_exists($file))
